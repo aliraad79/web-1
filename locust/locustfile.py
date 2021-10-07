7c84cf5465aae7dd3c7dc4e078830f2623ff3f6c
aliraad79 <a.ahmadi.k.79@gmail.com>
2021-10-07 20:15:25 +0330

    update locust

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -1,6 +1,13 @@
 from locust import HttpUser, task
 
+
 class HelloWorldUser(HttpUser):
     @task
-    def hello_world(self):
-        self.client.get("/")
+    def get_apis(self):
+        self.client.get("/node/sha256")
+        self.client.get("/go/sha256")
+
+    @task
+    def post_apis(self):
+        self.client.post("/node/sha256", {"Input": "AAAAAAAAAAAAAAAAAAA"})
+        self.client.post("/go/sha256", {"Input": "AAAAAAAAAAAAAAAAAAA"})
